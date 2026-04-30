d5ac932135139ae4bc2a6c52ee3b54ba651db256
giftF <1432002@qq.com>
2026-04-30 13:22:18 +0900

    Site updated: 2026-04-30 13:22:16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777520912294";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1777522931226";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
